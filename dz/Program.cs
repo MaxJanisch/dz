ad6fce075892c0253097dd238102bd888c97c06c
C5PC13 <C5PC13@C5PC13>
2023-09-06 19:54:02 +0200

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("введите x:");
+Console.WriteLine("введите y:");
+double x = double.Parse(Console.ReadLine());
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine($"V={Math.Log(y + 0.95) + Math.Sin((Math.Pow(x, 4) * Math.PI / 180))}");
